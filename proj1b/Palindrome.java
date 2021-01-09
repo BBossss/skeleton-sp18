027a84a5ae5ec60f303c4e9297afb47bdc78de73
BBossss <1551929492@qq.com>
2021-01-08 18:49:23 -0800

    correct style

--- proj1b/Palindrome.java
+++ proj1b/Palindrome.java
@@ -1,5 +1,5 @@
 public class Palindrome {
-    public Deque<Character> wordToDeque(String word){
+    public Deque<Character> wordToDeque(String word) {
         Deque<Character> d = new ArrayDeque<>();
         for (int i = 0; i < word.length(); i++) {
             d.addLast(word.charAt(i));

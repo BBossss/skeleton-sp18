131c4b2c60ec856cc4b02945f8d32ff28408fbd0
BBossss <1551929492@qq.com>
2021-01-09 18:35:07 -0800

    proj2 get started

--- proj2/byog/lab5/HexWorld.java
+++ proj2/byog/lab5/HexWorld.java
@@ -12,4 +12,44 @@ import java.util.Random;
  * Draws a world consisting of hexagonal regions.
  */
 public class HexWorld {
+
+    public void drawHexagon(int length) {
+        drawUp(length);
+        drawDown(length);
+    }
+
+    private void drawUp(int length) {
+        for (int i = 0; i < length; i++) {
+            drawLine(numberOfWhitespace(i + 1, length), ' ');
+            drawLine(numberOfTile(i + 1, length), 'a');
+            System.out.println();
+        }
+    }
+
+    private void drawDown(int length) {
+        for (int i = length; i > 0; i--) {
+            drawLine(numberOfWhitespace(i, length), ' ');
+            drawLine(numberOfTile(i, length), 'a');
+            System.out.println();
+        }
+    }
+
+    private int numberOfWhitespace(int row, int length) {
+        return length - row;
+    }
+
+    private int numberOfTile(int row, int length) {
+        return length + 2 * (row - 1);
+    }
+
+    private void drawLine(int length, char ch) {
+        for (int i = 0; i < length; i++) {
+            System.out.print(ch);
+        }
+    }
+
+    public static void main(String[] args) {
+        HexWorld hw = new HexWorld();
+        hw.drawHexagon(6);
+    }
 }
